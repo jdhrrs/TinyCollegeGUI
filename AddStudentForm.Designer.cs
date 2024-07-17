d249422e95651a700ed84e9d26f46273592d5d57
Justin Harris- Recently Graduated Eager Developer <okstateboy2015@gmail.com>
2024-07-17 06:55:57 -0500

    Fixed errors and warnings: Added missing event handlers, initialized fields, and corrected ambiguous methods in MainForm and AddStudentForm.

---
+++ AddStudentForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TinyCollegeGUI
+{
+    partial class AddStudentForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(12, 29);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(260, 20);
+            this.textBoxFirstName.TabIndex = 0;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(12, 68);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(260, 20);
+            this.textBoxLastName.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 105);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(12, 13);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(57, 13);
+            this.labelFirstName.TabIndex = 3;
+            this.labelFirstName.Text = "First Name";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(12, 52);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(58, 13);
+            this.labelLastName.TabIndex = 4;
+            this.labelLastName.Text = "Last Name";
+            // 
+            // AddStudentForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 141);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Name = "AddStudentForm";
+            this.Text = "Add Student";
+            this.Load += new System.EventHandler(this.AddStudentForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ AddStudentForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TinyCollegeGUI
+{
+    public partial class AddStudentForm : Form
+    {
+        public event Action<Student> StudentAdded;
+
+        public AddStudentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            // Generate a new student ID
+            int newStudentId = Database.GetNextStudentId(); // Assuming a method to get the next student ID
+            string firstName = textBoxFirstName.Text;
+            string lastName = textBoxLastName.Text;
+            double gpa = 0.0; // Default GPA
+
+            Student newStudent = new Student(newStudentId, firstName, lastName, gpa);
+
+            // Raise the event to notify the main form
+            StudentAdded?.Invoke(newStudent);
+
+            // Close the form
+            this.Close();
+        }
+
+        private void AddStudentForm_Load(object sender, EventArgs e)
+        {
+            // Perform any necessary initialization here
+        }
+    }
+}
