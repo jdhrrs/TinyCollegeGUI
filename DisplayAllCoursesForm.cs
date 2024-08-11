18d80c845f48c6c588d2c8d5b9a89b2123e3dc0d
JD Harris | Eager Student Developer |  Looking to make meaningful connections <51782837+jdhrrs@users.noreply.github.com>
2024-08-10 23:44:06 -0500

    Update DisplayAllCoursesForm.cs
    Update DisplayAllCoursesForm to use SQL Server instead of SQLite
    
    - Changed database connection to use SqlConnection
    - Updated SQL query to retrieve all courses
    - Modified parameter handling and connection string

--- DisplayAllCoursesForm.cs
+++ DisplayAllCoursesForm.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Data.SQLite;
-using System.Linq;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace TinyCollegeGUI
@@ -29,7 +28,7 @@ namespace TinyCollegeGUI
             {
                 connection.Open();
                 string query = "SELECT CourseID, CourseName, Credits FROM Courses"; // Explicitly select columns
-                using (var command = new SQLiteCommand(query, connection))
+                using (var command = new SqlCommand(query, connection))
                 {
                     using (var reader = command.ExecuteReader())
                     {
@@ -39,9 +38,9 @@ namespace TinyCollegeGUI
                             {
                                 var course = new Course
                                 {
-                                    CourseID = reader.GetInt32(0).ToString(), // Correcting CourseID to int and converting to string
-                                    CourseName = reader.GetString(1), // CourseName should be at index 1
-                                    Credits = reader.GetInt32(2)      // Credits should be at index 2
+                                    CourseID = reader.GetString(0),
+                                    CourseName = reader.GetString(1),
+                                    Credits = reader.GetInt32(2)
                                 };
                                 courses.Add(course);
                             }
@@ -70,10 +69,10 @@ namespace TinyCollegeGUI
         }
 
         // Get a connection to the database
-        private SQLiteConnection GetConnection()
+        private SqlConnection GetConnection()
         {
-            string connectionString = "Data Source=TinyCollege.db;Version=3;";
-            return new SQLiteConnection(connectionString);
+            string connectionString = ConfigurationManager.ConnectionStrings["TinyCollegeDB"].ConnectionString;
+            return new SqlConnection(connectionString);
         }
 
         // Search courses based on the text in the search box
@@ -99,6 +98,11 @@ namespace TinyCollegeGUI
         }
 
         private void dataGridViewCourses_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void DisplayAllCoursesForm_Load(object sender, EventArgs e)
         {
 
         }
