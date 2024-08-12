182e30b1cbd6ba5b4fd1c18441b3736e629b6743
JD Harris | Eager Student Developer |  Looking to make meaningful connections <51782837+jdhrrs@users.noreply.github.com>
2024-08-11 23:15:33 -0500

    Update WhoIsInACourseForm.Designer.cs Phils User Story 5
    Design WhoIsInACourseForm layout
    
    - Added UI elements for course ID input, find button, and DataGridView.
    - Added a close button.

--- WhoIsInACourseForm.Designer.cs
+++ WhoIsInACourseForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TinyCollegeGUI
+namespace TinyCollegeGUI
 {
     partial class WhoIsInACourseForm
     {
@@ -28,129 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            groupBox1 = new GroupBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            studentsListBox = new ListBox();
-            closeButton = new Button();
-            findButton = new Button();
-            groupBox1.SuspendLayout();
-            SuspendLayout();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.findButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.studentsListBox = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(findButton);
-            groupBox1.Controls.Add(closeButton);
-            groupBox1.Controls.Add(studentsListBox);
-            groupBox1.Controls.Add(textBox2);
-            groupBox1.Controls.Add(textBox1);
-            groupBox1.Controls.Add(label3);
-            groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(12, 12);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(776, 348);
-            groupBox1.TabIndex = 0;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "Course Data";
+            this.groupBox1.Controls.Add(this.findButton);
+            this.groupBox1.Controls.Add(this.closeButton);
+            this.groupBox1.Controls.Add(this.studentsListBox);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 348);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Course Data";
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(114, 41);
-            label1.Name = "label1";
-            label1.Size = new Size(106, 28);
-            label1.TabIndex = 0;
-            label1.Text = "Course ID:";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course ID:";
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(95, 97);
-            label2.Name = "label2";
-            label2.Size = new Size(125, 28);
-            label2.TabIndex = 1;
-            label2.Text = "Course Title:";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Course Title:";
             // 
             // label3
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(6, 153);
-            label3.Name = "label3";
-            label3.Size = new Size(342, 28);
-            label3.TabIndex = 2;
-            label3.Text = "All Students Enrolled In This Course:";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(342, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "All Students Enrolled In This Course:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(290, 101);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(262, 27);
-            textBox1.TabIndex = 3;
+            this.textBox1.Location = new System.Drawing.Point(290, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 27);
+            this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(290, 45);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 4;
+            this.textBox2.Location = new System.Drawing.Point(290, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 4;
             // 
             // studentsListBox
             // 
-            studentsListBox.FormattingEnabled = true;
-            studentsListBox.Location = new Point(6, 184);
-            studentsListBox.Name = "studentsListBox";
-            studentsListBox.Size = new Size(754, 104);
-            studentsListBox.TabIndex = 5;
+            this.studentsListBox.FormattingEnabled = true;
+            this.studentsListBox.ItemHeight = 20;
+            this.studentsListBox.Location = new System.Drawing.Point(6, 184);
+            this.studentsListBox.Name = "studentsListBox";
+            this.studentsListBox.Size = new System.Drawing.Size(754, 104);
+            this.studentsListBox.TabIndex = 5;
             // 
             // closeButton
             // 
-            closeButton.Location = new Point(321, 304);
-            closeButton.Name = "closeButton";
-            closeButton.Size = new Size(94, 29);
-            closeButton.TabIndex = 6;
-            closeButton.Text = "Cl&ose";
-            closeButton.UseVisualStyleBackColor = true;
-            closeButton.Click += closeButton_Click;
+            this.closeButton.Location = new System.Drawing.Point(321, 304);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(94, 29);
+            this.closeButton.TabIndex = 6;
+            this.closeButton.Text = "Cl&ose";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // findButton
             // 
-            findButton.Location = new Point(458, 45);
-            findButton.Name = "findButton";
-            findButton.Size = new Size(94, 29);
-            findButton.TabIndex = 7;
-            findButton.Text = "&Find";
-            findButton.UseVisualStyleBackColor = true;
+            this.findButton.Location = new System.Drawing.Point(458, 45);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(94, 29);
+            this.findButton.TabIndex = 7;
+            this.findButton.Text = "&Find";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
             // 
             // WhoIsInACourseForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(groupBox1);
-            Name = "WhoIsInACourseForm";
-            Text = "Who Is In A Course";
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "WhoIsInACourseForm";
+            this.Text = "Who Is In A Course";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
         }
 
         #endregion
 
-        private GroupBox groupBox1;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private Label label3;
-        private Label label2;
-        private Label label1;
-        private Button findButton;
-        private Button closeButton;
-        private ListBox studentsListBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.ListBox studentsListBox;
     }
 }
