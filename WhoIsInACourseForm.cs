9b2fbae059ebcc6ff52952a936fcceb40cf41fbd
JD Harris | Eager Student Developer |  Looking to make meaningful connections <51782837+jdhrrs@users.noreply.github.com>
2024-08-11 23:12:55 -0500

    Update WhoIsInACourseForm.cs Phils User Story 5
    Implement WhoIsInACourseForm to display students enrolled in a course
    
    - Added functionality to query and display students based on course ID.
    - Integrated with SQL Server.

--- WhoIsInACourseForm.cs
+++ WhoIsInACourseForm.cs
@@ -1,23 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 using System.Windows.Forms;
 
 namespace TinyCollegeGUI
 {
     public partial class WhoIsInACourseForm : Form
     {
+        private string connectionString;
+
+        // Constructor
         public WhoIsInACourseForm()
         {
             InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["TinyCollegeDB"].ConnectionString;
         }
 
-        private void closeButton_Click(object sender, EventArgs e)
+        // Method to get SQL connection
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        // Event handler for Find button click
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                string query = "SELECT Students.FirstName, Students.LastName " +
+                               "FROM Enrollments " +
+                               "JOIN Students ON Enrollments.StudentID = Students.StudentID " +
+                               "WHERE Enrollments.CourseID = @CourseID";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CourseID", txtCourseID.Text);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        dataGridViewStudents.DataSource = dt;
+                    }
+                }
+            }
+        }
+
+        // Event handler for Close button click
+        private void btnClose_Click(object sender, EventArgs e)
         {
             this.Close();
         }
