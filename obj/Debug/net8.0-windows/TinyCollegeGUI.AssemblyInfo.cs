68e7ebdb3acd5bcf418f05ddf9687908e5b06229
Justin Harris- Recently Graduated Eager Developer <okstateboy2015@gmail.com>
2024-08-13 02:49:51 -0500

    Extended the Main Form size so all buttons could be displayed and seen when program opens.

--- obj/Debug/net8.0-windows/TinyCollegeGUI.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/TinyCollegeGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TinyCollegeGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3da9a561aebb5bb44b7d661ac8f13e569919f0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f30ef2261366649ba43cc9a3a71fa340414ce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TinyCollegeGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TinyCollegeGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
