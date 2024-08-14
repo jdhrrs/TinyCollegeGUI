d13337dc28bb907c38a461da48f1b7934530a56a
madisonmato <madisonnessler13@gmail.com>
2024-08-13 21:11:44 -0500

    Update DeleteCourse.cs

--- DeleteCourse.cs
+++ DeleteCourse.cs
@@ -18,11 +18,13 @@ namespace TinyCollegeGUI
         {
             using SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TinyCollegeDB"].ConnectionString);
             using SqlDataAdapter adapter = new SqlDataAdapter(
+            // JOIN command used to combine information from two or more tables in a database - in this case, it it joining the Courses and Instructors tables
                 "SELECT * FROM Courses JOIN Instructors ON Courses.InstructorId = Instructors.InstructorId WHERE CourseID = @courseId", conn);
             adapter.SelectCommand.Parameters.AddWithValue("@courseId", textBoxCourseID.Text);
             DataTable courseTable = new DataTable();
             adapter.Fill(courseTable);
             if (courseTable.Rows.Count < 1)
+            // Tells system to display a message letting the user know if the course info they entered is not found in the database
             {
                 labelCourseNameValue.Text = "No course found";
                 labelInstructorNameValue.Text = string.Empty;
@@ -41,10 +43,12 @@ namespace TinyCollegeGUI
         private void buttonDelete_Click(object sender, EventArgs e)
         {
             using SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TinyCollegeDB"].ConnectionString);
+            // Deletes course from database when the delete button is clicked by user; DELETE FROM method tells the system where to delete the corresponding info from from the database, in this case, it is deleting from the Courses table
             using SqlCommand comd = new SqlCommand("DELETE FROM Courses WHERE CourseID = @courseId", conn);
             conn.Open();
             comd.Parameters.AddWithValue("@courseId", textBoxCourseID.Text);
             comd.ExecuteNonQuery();
+            // System creates a message box telling the user when the course is successfully deleted from the database
             MessageBox.Show("Course deleted.");
         }
 
