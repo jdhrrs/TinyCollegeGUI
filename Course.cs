65b829293db5d4b14d07ab6bb7c4f89abb5f2c02
madisonmato <madisonnessler13@gmail.com>
2024-08-13 21:02:06 -0500

    Update Course.cs

--- Course.cs
+++ Course.cs
@@ -7,14 +7,14 @@
         public int Credits { get; set; }
 
         public Course() { }
-
+        // gives values to the info that corresponds to the courses in the database, like the course ID, the name of the course, and how many credits the course is worth
         public Course(string courseId, string courseName, int credits)
         {
             CourseID = courseId;
             CourseName = courseName;
             Credits = credits;
         }
-
+        // override the ToString method in order to provide information about the type to client code
         public override string ToString()
         {
             return $"{CourseName} ({CourseID}) - {Credits} Credits";
