5a8c0e76b69d687591b1737800a60cc1804fe429
madisonmato <madisonnessler13@gmail.com>
2024-08-13 20:58:51 -0500

    Update AddStudentForm.cs

--- AddStudentForm.cs
+++ AddStudentForm.cs
@@ -14,18 +14,20 @@ namespace TinyCollegeGUI
         {
             InitializeComponent();
         }
-
+        // creates the event handler for the "Add Student" button, or in other words, makes the button functional
         private void buttonAddStudent_Click(object sender, EventArgs e)
         {
+        // the string.IsNullOrWHiteSpace method will return a boolean value. If the method’s parameter list is null or String.Empty, or only contains white-space characters then return True otherwise return False.
             if (string.IsNullOrWhiteSpace(textBoxFirstName.Text) ||
                 string.IsNullOrWhiteSpace(textBoxLastName.Text) ||
                 string.IsNullOrWhiteSpace(textBoxGPA.Text) ||
                 !double.TryParse(textBoxGPA.Text, out double gpa))
             {
+            // system creates a message box that tells the user to enter valid, or correct, student info
                 MessageBox.Show("Please enter valid student information.");
                 return;
             }
-
+            // gives values to text boxes on this form
             NewStudent = new Student
             {
                 FirstName = textBoxFirstName.Text,
